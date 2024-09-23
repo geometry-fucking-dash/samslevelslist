fba5b78f37bfa0cd092a15ffe7be234dd65f44e7
geometry-fucking-dash <95655098+geometry-fucking-dash@users.noreply.github.com>
2024-09-23 04:14:31 -0400

    Update List.js
    rule changing yada yada

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,13 +100,13 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        Achieved the record without any Pointercrate rule-breaking hacks. CBF is allowed on the grounds that you say so in the video description or level description.
                     </p>
                     <p>
                         Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Have either source audio or clicks/taps in the video. Volume readjustment is allowed so long as there are no clicks where the volume is adjusted
                     </p>
                     <p>
                         The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
@@ -121,7 +121,7 @@ export default {
                         Do not use easy modes, only a record of the unmodified level qualifies
                     </p>
                     <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
+                        Once a level falls onto the Legacy List, records will be accepted but you will not gain any list points.
                     </p>
                 </div>
             </div>
